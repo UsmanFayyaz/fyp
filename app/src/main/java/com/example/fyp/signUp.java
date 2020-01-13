2c59964835f8fc783bdcad58cc0b6311c1ac8b4d
Usman Fayyaz <usman.fayyaz@ucp.edu.pk>
2020-01-13 19:21:40 +0500

    .

--- app/src/main/java/com/example/fyp/signUp.java
+++ app/src/main/java/com/example/fyp/signUp.java
@@ -13,7 +13,6 @@ public class signUp extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.sign_up, container, false);
-        return view;
+        return inflater.inflate(R.layout.sign_up, container, false);
     }
 }
