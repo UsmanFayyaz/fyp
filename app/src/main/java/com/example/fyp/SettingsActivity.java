00425d2f20a60ca585486fb9730f6af1e5522d38
Usman Fayyaz <usman.fayyaz@ucp.edu.pk>
2020-01-19 22:56:32 +0500

    setting activity

--- app/src/main/java/com/example/fyp/SettingsActivity.java
+++ app/src/main/java/com/example/fyp/SettingsActivity.java
@@ -3,6 +3,8 @@ package com.example.fyp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class SettingsActivity extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class SettingsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        String[] fruits = {"Apple", "Mango", "Orange", "Grape"};
+
+        ArrayAdapter<String> myArrayAdapter = new ArrayAdapter<String>(this,R.layout.activity_setting_listview,R.id.list,fruits);
+        ListView myListView = (ListView) findViewById(R.id.myList);
+        myListView.setAdapter(myArrayAdapter);
     }
 }
