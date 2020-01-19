7766f71a97f5ef4daecdf258c820d5839a7eb811
Usman Fayyaz <usman.fayyaz@ucp.edu.pk>
2020-01-19 23:09:19 +0500

    setting activity

--- app/src/main/java/com/example/fyp/SettingsActivity.java
+++ app/src/main/java/com/example/fyp/SettingsActivity.java
@@ -1,50 +1,22 @@
 package com.example.fyp;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Context;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.ImageView;
 import android.widget.ListView;
-import android.widget.TextView;
-import android.widget.Toast;
 
-import org.w3c.dom.Text;
-
-public class SettingsActivity extends AppCompatActivity  implements AdapterView.OnItemClickListener {
-    ListView l1;
-
-    String[] settings_activity={"FontSzie", "Font Color", "Profile Info", "Change User Name & Password"};
-
+public class SettingsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
 
-        l1 = findViewById(R.id.listview);
+        String[] fruits = {"Apple", "Mango", "Orange", "Grape"};
 
-        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1);
-        l1.setAdapter(arrayAdapter);
-        l1.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-
-            }
-        })
-;}
-
-    @Override
-    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-
-        TextView tv=(TextView)view;
-        Toast.makeText(this,"you click"+tv.getText(),Toast.LENGTH_SHORT).show();
+        ArrayAdapter myArrayAdapter = new ArrayAdapter<String>(this, R.layout.activity_setting_listview, R.id.list, fruits);
+        ListView myListView = (ListView) findViewById(R.id.myList);
+        myListView.setAdapter(myArrayAdapter);
     }
 }
